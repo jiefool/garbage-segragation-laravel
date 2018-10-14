7e3cb6be991e22b2591c83f974bb39cf1b45564c
ivankrister <ivankrister.garica@gmail.com>
2018-10-14 11:30:23 +0800

    sms

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -15,43 +15,95 @@ class LevelController extends Controller
     public function store(){
 
 
+           
 
 
-       	
-       	$level = request('level');
-       	$centi = 0;
-       	if($level == 1){
+            
+            $level = request('level');
+            $centi = 0;
+            if($level == 1){
 
-       	$centi = rand(100,150);
-       	}
-       	elseif ($level == 2) {
-       	$centi = rand(151,200);
-       	
-       	}
-       	elseif ($level == 3) {
-       	$centi = rand(201,250);
-       	
-       	}
-       	elseif ($level == 4) {
-       	$centi = rand(251,300);
-       	
-       	}
-       	elseif ($level == 5) {
-       	$centi = rand(301,400);
-       	
-       	}
-       	else{
-       	$centi = rand(401,700);
-       	
+            $centi = rand(15,20);
+            }
+            elseif ($level == 2) {
+            $centi = rand(35,40);
+            
+            }
+            elseif ($level == 3) {
+            $centi = rand(55,60);
+            
+            }
+            elseif ($level == 4) {
+            $centi = rand(75,80);
 
-       	}
 
-       	$data = new \App\Level;
-       	$data->number = $level;
-       	$data->centimeter = $centi;
-       	$data->save();
+            $contactList = \App\Contact::all();
+            $contacts = [];
 
-       	return $data;
+            foreach ($contactList as $contact) {
+
+
+                  $sendMessageRequest = new SendMessageRequest([
+                      'phoneNumber' => $contact->contact,
+                      'message' => 'Ang tubig ning aksyon na ug saka pag bantay bantay namo',
+                      'deviceId' => 96102
+                  ]);
+
+                  array_push($contacts, $sendMessageRequest);
+
+            }
+            $sendMessages = $messageClient->sendMessages($contacts);
+            
+            }
+            elseif ($level == 5) {
+            $centi = rand(95,100);
+
+            $contactList = \App\Contact::all();
+            $contacts = [];
+
+            foreach ($contactList as $contact) {
+
+
+                  $sendMessageRequest = new SendMessageRequest([
+                      'phoneNumber' => $contact->contact,
+                      'message' => 'Palihug uli na inyong balay kay hapit na mobaha',
+                      'deviceId' => 96102
+                  ]);
+
+                  array_push($contacts, $sendMessageRequest);
+
+            }
+            $sendMessages = $messageClient->sendMessages($contacts);
+
+            
+            }
+            else{
+            $centi = rand(110,200);
+            $contactList = \App\Contact::all();
+            $contacts = [];
+
+            foreach ($contactList as $contact) {
+
+
+                  $sendMessageRequest = new SendMessageRequest([
+                      'phoneNumber' => $contact->contact,
+                      'message' => 'Pahibalo dagan namo kay ang tubig ning nasaka na!',
+                      'deviceId' => 96102
+                  ]);
+
+                  array_push($contacts, $sendMessageRequest);
+
+            }
+            $sendMessages = $messageClient->sendMessages($contacts);
+
+            }
+
+            $data = new \App\Level;
+            $data->number = $level;
+            $data->centimeter = $centi;
+            $data->save();
+
+            return $data;
 
 
     }
